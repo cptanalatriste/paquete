8988801dac606224bb542f128541f40fd42b9dd9
Carlos Gavidia-Calderon <cgavidia-calderon@turing.ac.uk>
2024-09-03 14:02:12 +0100

    First commit

---
+++ src/paquete/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about paquete."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Carlos Gavidia-Calderon"
+__email__ = "carlos@gavidia.me"
+__version__ = "0.1.0"
